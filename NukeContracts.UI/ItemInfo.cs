47308115038446787b53102e2cd1e0c2aa5c237e
nukeLEARgames <lmhaulsee@gmail.com>
2018-08-27 10:44:46 -0400

    created user controls form info panel

---
+++ NukeContracts.UI/ItemInfo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NukeContracts.UI
+{
+    public partial class ItemInfo : UserControl
+    {
+        public ItemInfo()
+        {
+            InitializeComponent();
+        }
+    }
+}
