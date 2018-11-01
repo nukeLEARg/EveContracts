2b3e1ba690a147a6433d7e35c4cb816aaf12066f
nukeLEARgames <lmhaulsee@gmail.com>
2018-11-01 10:29:59 -0400

    added dynamics again

---
+++ NukeContracts.Business/Enumerations/Dynamics.cs
@@ -0,0 +1,44 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NukeContracts.Business.Enumerations
+{
+    public enum Dynamics
+    {
+        [Display(Name = "50MN Abyssal Microwarpdrive")] _50MN = 47408,
+        [Display(Name = "Abyssal Stasis Webifier")] Webifier = 47702,
+        [Display(Name = "Abyssal Warp Scrambler")] Scramber = 47732,
+        [Display(Name = "Abyssal Warp Disruptor")] Disruptor = 47736,
+        [Display(Name = "5MN Abyssal Microwarpdrive")] _5MN = 47740,
+        [Display(Name = "500MN Abyssal Microwarpdrive")] _500MN = 47745,
+        [Display(Name = "1MN Abyssal Afterburner")] _1MN = 47749,
+        [Display(Name = "10MN Abyssal Afterburner")] _10MN = 47753,
+        [Display(Name = "100MN Abyssal Afterburner")] _100MN = 47757,
+        [Display(Name = "Small Abyssal Armor Repairer")] SmallArmorRep = 47769,
+        [Display(Name = "Medium Abyssal Armor Repairer")] MediumArmorRep = 47773,
+        [Display(Name = "Large Abyssal Armor Repairer")] LargeArmorRep = 47777,
+        [Display(Name = "Small Abyssal Shield Booster")] SmallShieldBoost = 47781,
+        [Display(Name = "Medium Abyssal Shield Booster")] MediumShieldBoost = 47785,
+        [Display(Name = "Large Abyssal Shield Booster")] LargeShieldBoost = 47789,
+        [Display(Name = "X-Large Abyssal Shield Booster")] XLargeShieldBoost = 47793,
+        [Display(Name = "Small Abyssal Shield Extender")] SmallShieldExtender = 47800,
+        [Display(Name = "Medium Abyssal Shield Extender")] MediumShieldExtender = 47804,
+        [Display(Name = "Large Abyssal Shield Extender")] LargeShieldExtender = 47808,
+        [Display(Name = "Small Abyssal Armor Plates")] SmallArmorPlate = 47812,
+        [Display(Name = "Medium Abyssal Armor Plates")] MediumArmorPlate = 47817,
+        [Display(Name = "Large Abyssal Armor Plates")] LargeArmorPlate = 47820,
+        [Display(Name = "Small Abyssal Energy Neutralizer")] SmallNeut = 47824,
+        [Display(Name = "Medium Abyssal Energy Neutralizer")] MediumNeut = 47828,
+        [Display(Name = "Heavy Abyssal Energy Neutralizer")] HeavyNeut = 47832,
+        [Display(Name = "Medium Abyssal Ancillary Shield Booster")] MediumASB = 47836,
+        [Display(Name = "Large Abyssal Ancillary Shield Booster")] LargeASB = 47838,
+        [Display(Name = "X-Large Abyssal Ancillary Shield Booster")] XLargeASB = 47840,
+        [Display(Name = "Small Abyssal Ancillary Armor Repairer")] SmallAAR = 47842,
+        [Display(Name = "Medium Abyssal Ancillary Armor Repairer")] MediumAAR = 47844,
+        [Display(Name = "Large Abyssal Ancillary Armor Repairer")] LargeAAR = 47846,
+        [Display(Name = "Medium Abyssal Energy Nosferatu")] MediumNos = 48423,
+        [Display(Name = "Heavy Abyssal Energy Nosferatu")] HeavyNos = 48427,
+        [Display(Name = "Small Abyssal Cap Battery")] SmallCapBattery = 48431,
+        [Display(Name = "Medium Abyssal Cap Battery")] MediumCapBattery = 48435,
+        [Display(Name = "Large Abyssal Cap Battery")] LargeCapBattery = 48439,
+    }
+}
